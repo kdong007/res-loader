ec143ac97c85b0d254e80183749a860bb68b9895
Mars Dong <kdong007@hotmail.com>
2019-08-21 16:34:12 +0800

    ResLoader

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+interface ResLoaderConfig<DataType> {
+    fetchRemote: (string) => Promise<DataType>;
+    fetchLocal: (string) => Promise<DataType>;
+    saveLocal: (string, DataType) => Promise<any>;
+}
+
+export default class ResLoader<DataType> {
+    config: ResLoaderConfig<DataType>;
+    actions: { [key: string]: Promise<DataType> } = {};
+
+    constructor(config: ResLoaderConfig<DataType>) {
+        this.config = config;
+    }
+
+    load(key: string): Promise<DataType> {
+        if (this.actions[key]) {
+            return this.actions[key];
+        }
+        const action = this._load(key);
+        this.actions[key] = action;
+        action.catch(() => (this.actions[key] = null));
+        return action;
+    }
+
+    async _load(key: string): Promise<DataType> {
+        const localData: DataType = await this.config.fetchLocal(key);
+        if (localData) {
+            return localData;
+        }
+
+        const remoteData: DataType = await this.config.fetchRemote(key);
+        this.config.saveLocal(key, remoteData); // no wait
+
+        return remoteData;
+    }
+}
