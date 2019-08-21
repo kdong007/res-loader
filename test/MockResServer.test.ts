df63bf6c16ac2545497c38e82124cf89cfc563d1
Mars Dong <kdong007@hotmail.com>
2019-08-21 15:01:16 +0800

    move fake timer

--- test/MockResServer.test.ts
+++ test/MockResServer.test.ts
@@ -1,5 +1,5 @@
 import MockResServer from "./MockResServer";
-jest.useFakeTimers();
+
 test("create instance", () => {
     const server: MockResServer = new MockResServer(100);
     expect(server).not.toBeNull();
@@ -11,6 +11,8 @@ test("load some data", () => {
 });
 
 test("load time", async () => {
+    jest.useFakeTimers();
+
     const server: MockResServer = new MockResServer(100);
     const spyFn = jest.fn();
     server.load(20).then(spyFn);
